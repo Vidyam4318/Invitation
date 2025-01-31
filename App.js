04a568c252742a455cd27c1ff7ee4258905444f6
Vidya <114570375+Vidyam4318@users.noreply.github.com>
2025-01-31 16:12:42 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,14 @@
+// src/App.js
+import React from 'react';
+import SendInvitations from './SendInvitations';
+
+const App = () => {
+    return (
+        <div className="App">
+            <h1>Invitation System</h1>
+            <SendInvitations />
+        </div>
+    );
+};
+
+export default App;
